afe622231ac1d00f25f2a96796cb6c6dedc6cb9b
Mikko Syrjä <mikko@localhost.localdomain>
2022-05-28 23:17:50 +0300

    Update source copyrights.

--- keypad.cpp
+++ keypad.cpp
@@ -1,6 +1,6 @@
 // This file is part of the SpeedCrunch project
 // Copyright (C) 2014 @qwazix
-// Copyright (C) 2018 Mikko Syrjä
+// Copyright (C) 2018-2022 Mikko Syrjä
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
